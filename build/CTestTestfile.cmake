97f44b484f711d24dbc26ab61c1a7f1740aa90a0
PKramek <peterkramek@gmail.com>
2024-11-04 17:41:08 +0000

    Remove build files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /workspaces/cpp-devcontainer
-# Build directory: /workspaces/cpp-devcontainer/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/workspaces/cpp-devcontainer/build/calculator_tests[1]_include.cmake")
-subdirs("_deps/googletest-build")
